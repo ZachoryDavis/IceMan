fd37dbb7f00f54e99662d8a6800260cdaba6cfa1
noahkim006 <noahkim0981@gmail.com>
2025-05-14 12:44:28 -0700

    created basic outline to some class constructor and fixed some small typos

--- IceMan-skeleton-windows/IceMan/IceMan/Actor.h
+++ IceMan-skeleton-windows/IceMan/IceMan/Actor.h
@@ -4,98 +4,111 @@
 #include "GraphObject.h"
 
 class Actor {
-public:
-	virtual ~Actor();
+	public:
+		virtual ~Actor();
+		virtual void doSomething() = 0;
+
+		private:
+			
 };
 
 
 class Human : public Actor {
-public:
-	virtual ~Human();
+	public:
+		virtual ~Human();
+	private:
+		int health{};
+		
 };
 
 
 class Solid : public Actor {
-public:
-	virtual ~Solid();
+	public:
+		virtual ~Solid();
 };
 
 
 class Goodie : public Actor {
-public:
-	virtual ~Goodie();
+	public:
+		virtual ~Goodie();
 };
 
 
 class IceMan : public Human {
-public:
-	virtual ~IceMan();
+	public:
+		IceMan();
+		IceMan(int startX = 30, int startY = 60, int imageID = IID_PLAYER, Direction dir = right, double size = 1.0, unsigned int depth = 0, int health = 10);
+		virtual ~IceMan();
+	private:
+		int numberOfSquirts = 5;
+		int numberOfSonarKits = 1;
+		int numberOfGoldNuggets = 0;
 };
 
 
 class Protester : public Human {
-public:
-	virtual ~Protester();
+	public:
+		virtual ~Protester();
 };
 
 
-class boulder : public Solid {
-public:
-	~Boulder();
+class Boulder : public Solid {
+	public:
+		~Boulder();
 };
 
 
 class Ice : public Solid {
-public:
-	~Ice();
+	public:
+		~Ice();
 };
 
 
-class RegularProtester : public protester {
-public:
-	~RegularProtester();
+class RegularProtester : public Protester {
+	public:
+		~RegularProtester();
 };
 
 
-class HardcoreProtester : public protester {
-public:
-	~HardcoreProtester();
+class HardcoreProtester : public Protester {
+	public:
+		~HardcoreProtester();
 };
 
 
 class Squirts : public IceMan {
 	//Could just be a public Solid but pretty sure its a subclass
 	//of IceMan
-public:
-	~Squirts();
+	public:
+		~Squirts();
 };
 
 
 class Water : public Goodie {
 	//Could be under Solid instead of Goodie if water is
 	//not a pick-upable item
-public:
-	~Water();
+	public:
+		~Water();
 };
 
 
 class SonarKit : public Goodie {
-public:
-	~SonarKit();
+	public:
+		~SonarKit();
 };
 
 
 class GoldNugget : public Goodie {
-public:
-	~GoldNugget();
+	public:
+		~GoldNugget();
 };
 
 
 class BarrelOfOil : public Goodie {
 	//Could be under Solid instead of Goodie if Barrel of Oil is
 	//not a pick-upable item
-public:
-	~BarrelOfOil();
+	public:
+		~BarrelOfOil();
 };
 
 #endif // ACTOR_H_

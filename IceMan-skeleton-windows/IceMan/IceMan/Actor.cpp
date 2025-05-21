75ef25346e0ccd3912f2f7971c14fe9d2b36eb91
noahkim006 <noahkim0981@gmail.com>
2025-05-21 15:54:56 -0700

    rearranged actor h file to make ti less messy, started implementation for constructor

--- IceMan-skeleton-windows/IceMan/IceMan/Actor.cpp
+++ IceMan-skeleton-windows/IceMan/IceMan/Actor.cpp
@@ -1,4 +1,34 @@
 #include "Actor.h"
 #include "StudentWorld.h"
 
-// Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
+
+Actor::Actor(int imageID, int startX, int startY, Direction startingDirection, double size, unsigned int depth, StudentWorld* studentWorld) 
+	: GraphObject(imageID, startX, startY, startingDirection, size, depth) {
+
+	this->studentWorld = studentWorld;
+	this->alive = true;
+}
+
+Actor::~Actor() {
+	//TODO: IMPLEMENT
+}
+
+Human::Human(int imageID, int startX, int startY, Direction startingDirection, double size, unsigned int depth, StudentWorld* studentWorld, int health)
+	: Actor(imageID, startX, startY, startingDirection, size, depth, studentWorld) {
+
+	
+}
+
+Human::~Human() {
+	//TODO: IMPLEMENT
+}
+
+IceMan::IceMan(int imageID, int startX, int startY, Direction startingDirection, double size, unsigned int depth, StudentWorld* studentWorld, int health)
+	: Human(imageID, startX, startY, startingDirection, size, depth, studentWorld, health) {
+	 //where should we declare heallth??
+}
+
+IceMan::~IceMan() {
+	//TODO: IMPLEMENT
+}
+
--- IceMan-skeleton-windows/IceMan/IceMan/Actor.h
+++ IceMan-skeleton-windows/IceMan/IceMan/Actor.h
@@ -3,143 +3,148 @@
 
 #include "GraphObject.h"
 
-<<<<<<< Updated upstream
-class Actor {
-	public:
-		virtual ~Actor();
-		virtual void doSomething() = 0;
+class StudentWorld;
 
-		private:
-			
-=======
+
+
+
 class Actor : public GraphObject{
+
+private:
+	StudentWorld* studentWorld;
+	bool alive;
 public:
-	GraphObject(int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0)
+	Actor(int imageID, int startX, int startY, Direction startingDirection, double size, unsigned int depth, StudentWorld* studentWorld);
 	virtual ~Actor();
->>>>>>> Stashed changes
 };
 
 
+
+//============== HUMAN CLASSES ===================
 class Human : public Actor {
-	public:
-		virtual ~Human();
-	private:
-		int health{};
+private:
+	int health{};
+public:
+	Human(int imageID, int startX, int startY, Direction startingDirection, double size, unsigned int depth, StudentWorld* studentWorld, int health);
+	virtual ~Human();
 		
 };
 
-
-class Solid : public Actor {
-	public:
-		virtual ~Solid();
-};
-
-
-class Goodie : public Actor {
-	public:
-		virtual ~Goodie();
-};
-
-
 class IceMan : public Human {
-	public:
-		IceMan();
-		IceMan(int startX = 30, int startY = 60, int imageID = IID_PLAYER, Direction dir = right, double size = 1.0, unsigned int depth = 0, int health = 10);
-		virtual ~IceMan();
-	private:
-		int numberOfSquirts = 5;
-		int numberOfSonarKits = 1;
-		int numberOfGoldNuggets = 0;
-};
-
-
-class Protester : public Human {
-	public:
-		virtual ~Protester();
-};
-
-
-class Boulder : public Solid {
-<<<<<<< Updated upstream
-	public:
-		~Boulder();
-=======
+private:
+	int numberOfSquirts = 5;
+	int numberOfSonarKits = 1;
+	int numberOfGoldNuggets = 0;
+	//int iceManHealth;
 public:
-	~Boulder();
->>>>>>> Stashed changes
+	IceMan(int imageID, int startX, int startY, Direction startingDirection, double size, unsigned int depth, StudentWorld* studentWorld, int health);
+	virtual ~IceMan();
+
 };
 
 
-class Ice : public Solid {
-<<<<<<< Updated upstream
-	public:
-		~Ice();
-=======
-public:
-	Ice(int startX, int startY, StudentWorld* sw) : Actor(IID_ICE, startX, startY, sw, right, 0.25, 3)
-	{
-		setVisible(true);
-	}
-	~Ice();
->>>>>>> Stashed changes
-};
 
 
-class RegularProtester : public Protester {
-<<<<<<< Updated upstream
-	public:
-		~RegularProtester();
-=======
-public:
-	~RegularProtester();
->>>>>>> Stashed changes
-};
+//================================================
 
 
-class HardcoreProtester : public Protester {
-<<<<<<< Updated upstream
-	public:
-		~HardcoreProtester();
-=======
-public:
-	~HardcoreProtester();
->>>>>>> Stashed changes
-};
 
-
-class Squirts : public IceMan {
-	//Could just be a public Solid but pretty sure its a subclass
-	//of IceMan
-	public:
-		~Squirts();
-};
-
-
-class Water : public Goodie {
-	//Could be under Solid instead of Goodie if water is
-	//not a pick-upable item
-	public:
-		~Water();
-};
-
-
-class SonarKit : public Goodie {
-	public:
-		~SonarKit();
-};
-
-
-class GoldNugget : public Goodie {
-	public:
-		~GoldNugget();
-};
-
-
-class BarrelOfOil : public Goodie {
-	//Could be under Solid instead of Goodie if Barrel of Oil is
-	//not a pick-upable item
-	public:
-		~BarrelOfOil();
-};
+//
+//
+//class Solid : public Actor {
+//	public:
+//		virtual ~Solid();
+//};
+//
+//
+//class Goodie : public Actor {
+//	public:
+//		virtual ~Goodie();
+//};
+//
+//
+//class IceMan : public Human {
+//	public:
+//		IceMan();
+//		IceMan(int startX = 30, int startY = 60, int imageID = IID_PLAYER, Direction dir = right, double size = 1.0, unsigned int depth = 0, int health = 10);
+//		virtual ~IceMan();
+//	private:
+//		int numberOfSquirts = 5;
+//		int numberOfSonarKits = 1;
+//		int numberOfGoldNuggets = 0;
+//};
+//
+//
+//class Protester : public Human {
+//	public:
+//		virtual ~Protester();
+//};
+//
+//
+//class Boulder : public Solid {
+//
+//public:
+//	~Boulder();
+//};
+//
+//
+//class Ice : public Solid {
+//public:
+//	/*Ice(int startX, int startY, StudentWorld* sw) : Actor(IID_ICE, startX, startY, sw, right, 0.25, 3)
+//	{
+//		setVisible(true);
+//	}*/
+//
+//	Ice(int startX, int startY, StudentWorld* studentWorld) : Solid()
+//	~Ice();
+//};
+//
+//
+//class RegularProtester : public Protester {
+//public:
+//	~RegularProtester();
+//};
+//
+//
+//class HardcoreProtester : public Protester {
+//public:
+//	~HardcoreProtester();
+//};
+//
+//
+//class Squirts : public IceMan {
+//	//Could just be a public Solid but pretty sure its a subclass
+//	//of IceMan
+//	public:
+//		~Squirts();
+//};
+//
+//
+//class Water : public Goodie {
+//	//Could be under Solid instead of Goodie if water is
+//	//not a pick-upable item
+//	public:
+//		~Water();
+//};
+//
+//
+//class SonarKit : public Goodie {
+//	public:
+//		~SonarKit();
+//};
+//
+//
+//class GoldNugget : public Goodie {
+//	public:
+//		~GoldNugget();
+//};
+//
+//
+//class BarrelOfOil : public Goodie {
+//	//Could be under Solid instead of Goodie if Barrel of Oil is
+//	//not a pick-upable item
+//	public:
+//		~BarrelOfOil();
+//};
 
 #endif // ACTOR_H_

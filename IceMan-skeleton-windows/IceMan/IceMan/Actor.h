0526356233da3ffe8cb61b1b2d7ecdc62611f572
noahkim006 <noahkim0981@gmail.com>
2025-05-21 15:57:59 -0700

    made actor h file more readable and organized, started implementation into construcs

--- IceMan-skeleton-windows/IceMan/IceMan/Actor.h
+++ IceMan-skeleton-windows/IceMan/IceMan/Actor.h
@@ -42,109 +42,82 @@ public:
 
 };
 
+class RegularProtestor : public Human {
+private:
+public:
+	//TODO: IMPLEMENT CONSTRUCTOR AND CLASS VARS
+	RegularProtestor();
+	virtual ~RegularProtestor();
+};
+
+//Inherit from RegularProtestor or Human ????
+class HardcoreProtestor : public RegularProtestor {
+private:
+public:
+	//TODO: IMPLEMENT
+};
 
 
 
 //================================================
 
+//============= SOLID CLASSES ====================
+
+class Solid : public Actor {
+private:
+
+public:
+
+};
+
+class Ice : public Solid {
+private:
+public:
+	
+};
+
+class Boulder : public Solid {
+private:
+public:
+};
+
+//================================================
+
+//============= MISC CLASSES =====================
+
+class Goodie : public Actor {
+private:
+public:
+
+};
+
+class Water : public Goodie {
+private:
+public:
+};
+
+class Sonar : public Goodie {
+private:
+public:
+};
+
+class Gold : public Goodie {
+private:
+public:
+};
+
+class OilBarrel : public Goodie {
+private:
+public:
+};
 
 
+//wtf do we do with this
+//class Squirt : public IceMan {
 //
-//
-//class Solid : public Actor {
-//	public:
-//		virtual ~Solid();
 //};
-//
-//
-//class Goodie : public Actor {
-//	public:
-//		virtual ~Goodie();
-//};
-//
-//
-//class IceMan : public Human {
-//	public:
-//		IceMan();
-//		IceMan(int startX = 30, int startY = 60, int imageID = IID_PLAYER, Direction dir = right, double size = 1.0, unsigned int depth = 0, int health = 10);
-//		virtual ~IceMan();
-//	private:
-//		int numberOfSquirts = 5;
-//		int numberOfSonarKits = 1;
-//		int numberOfGoldNuggets = 0;
-//};
-//
-//
-//class Protester : public Human {
-//	public:
-//		virtual ~Protester();
-//};
-//
-//
-//class Boulder : public Solid {
-//
-//public:
-//	~Boulder();
-//};
-//
-//
-//class Ice : public Solid {
-//public:
-//	/*Ice(int startX, int startY, StudentWorld* sw) : Actor(IID_ICE, startX, startY, sw, right, 0.25, 3)
-//	{
-//		setVisible(true);
-//	}*/
-//
-//	Ice(int startX, int startY, StudentWorld* studentWorld) : Solid()
-//	~Ice();
-//};
-//
-//
-//class RegularProtester : public Protester {
-//public:
-//	~RegularProtester();
-//};
-//
-//
-//class HardcoreProtester : public Protester {
-//public:
-//	~HardcoreProtester();
-//};
-//
-//
-//class Squirts : public IceMan {
-//	//Could just be a public Solid but pretty sure its a subclass
-//	//of IceMan
-//	public:
-//		~Squirts();
-//};
-//
-//
-//class Water : public Goodie {
-//	//Could be under Solid instead of Goodie if water is
-//	//not a pick-upable item
-//	public:
-//		~Water();
-//};
-//
-//
-//class SonarKit : public Goodie {
-//	public:
-//		~SonarKit();
-//};
-//
-//
-//class GoldNugget : public Goodie {
-//	public:
-//		~GoldNugget();
-//};
-//
-//
-//class BarrelOfOil : public Goodie {
-//	//Could be under Solid instead of Goodie if Barrel of Oil is
-//	//not a pick-upable item
-//	public:
-//		~BarrelOfOil();
-//};
+// 
+//=================================================
+
 
 #endif // ACTOR_H_
